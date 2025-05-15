36d41d14fa24515cb03d4abbe67c6a80fe82e8e8
Иван Якимов <sasha2020419@gmail.com>
2025-05-15 22:20:56 +0300

    test

--- index.js
+++ index.js
@@ -29,3 +29,4 @@ app.use('/api/appointments', authenticateJWT, appointmentsRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+ sdfsd
--- routes/appointments.js
+++ routes/appointments.js
@@ -49,5 +49,5 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
-
+// TODO: test
 module.exports = router;
